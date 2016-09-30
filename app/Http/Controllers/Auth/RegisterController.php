50919b1f931ebe76b5fc213cf0604a997701cb73
rjdyx <1191200688@qq.com>
2016-09-30 19:20:38 +0800

    [MOD]将登陆注册改成异步请求

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -42,22 +42,33 @@ class RegisterController extends Controller
     /**
      * Get a validator for an incoming registration request.
      *
-     * @param  array  $data
+     * @param  array $data
      * @return \Illuminate\Contracts\Validation\Validator
      */
     protected function validator(array $data)
     {
+        $errorTips = [
+            'name.required' => '用户名必填',
+            'name.max' => '用户名长度不能超过255',
+            'email.required' => '邮箱必填',
+            'email.email' => '邮箱格式错误',
+            'email.max' => '邮箱长度不能超过255',
+            'email.unique' => '邮箱已存在',
+            'password.required' => '密码必填',
+            'password.min' => '密码长度不能小于6',
+            'password.confirmed' => '密码不一致',
+        ];
         return Validator::make($data, [
             'name' => 'required|max:255',
             'email' => 'required|email|max:255|unique:users',
             'password' => 'required|min:6|confirmed',
-        ]);
+        ], $errorTips);
     }
 
     /**
      * Create a new user instance after a valid registration.
      *
-     * @param  array  $data
+     * @param  array $data
      * @return User
      */
     protected function create(array $data)
